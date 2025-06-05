a6c11f781bfa6f1df7edf8dc45f0ed3c449c4586
AetherSwap <aetherswap@proton.me>
2025-06-05 18:31:29 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+// --- Variables globales ---
+const topCoins = [
+  { id: 'solana', symbol: 'SOL', name: 'Solana', logo: 'https://cryptologos.cc/logos/solana-sol-logo.svg?v=024' },
+  { id: 'bitcoin', symbol: 'BTC', name: 'Bitcoin', logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=024' },
+  { id: 'ethereum', symbol: 'ETH', name: 'Ethereum', logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=024' },
+  { id: '
